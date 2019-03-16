bf5991b3bf8f2296a1ab070dd5f1593503096989
Chris Strutton <chris@rodandfly.ca>
2019-03-16 15:19:47 -0400

    create todo component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,29 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-toolbar app color="indigo" dark>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        Todo App
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn flat>
+        <span class="mr-2">Register/Login</span>
+        <v-icon>mdi-account</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <todo-component>
+      </todo-component>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Todo from './components/Todo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'todo-component': Todo
   },
   data () {
     return {
---
+++ src/components/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="indigo">
+            <v-toolbar-title>The List</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon large>
+              <v-icon large>mdi-plus</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-card-text>
+            <ul>
+              <li v-for="(item, index) in items" v-bind:key='index'>
+                {{ index }} - {{ item.text }}
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { text: 'item 1' },
+        { text: 'item 2' },
+        { text: 'item 3' }
+      ]
+    })
+  }
+</script>
+
+<style>
+
+</style>
